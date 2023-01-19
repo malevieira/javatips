f3aa03dc91e149bc17153a7bf83a579129fd5cc1
Mauricio Vieira <malevieira@gmail.com>
2023-01-19 18:33:07 -0300

    tips: Add new class PassByValueOrReference

---
+++ src/main/java/com/malevieira/tips/PassByValueOrReference.java
@@ -0,0 +1,43 @@
+package com.malevieira.tips;
+
+class BaseClass {
+
+    private Integer levelOfPerformance = 0;
+
+    public BaseClass() { }
+
+    public BaseClass(Integer levelOfPerformance) {
+        this.levelOfPerformance = levelOfPerformance;
+    }
+
+    public Integer getLevelOfPerformance() {
+        return levelOfPerformance;
+    }
+
+    public void setLevelOfPerformance(Integer levelOfPerformance) {
+        this.levelOfPerformance = levelOfPerformance;
+    }
+}
+
+public class PassByValueOrReference {
+
+    public static void main(String[] args) {
+
+        BaseClass myBaseClass = new BaseClass();
+        myBaseClass.setLevelOfPerformance(10);
+
+        System.out.println("Before increase : " + myBaseClass);
+        increaseLevelOfPerformance(myBaseClass);
+        System.out.println(myBaseClass.getLevelOfPerformance());
+
+    }
+
+    private static void increaseLevelOfPerformance(BaseClass baseClass) {
+
+        baseClass = new BaseClass();
+        // the below code doesn't impact the parameter reference
+        // because it will receive a new reference from (new BaseClass() instantiation)
+        baseClass = new BaseClass();
+        baseClass.setLevelOfPerformance(756);
+    }
+}
